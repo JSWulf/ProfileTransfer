3f9b12e90fa653c6e0dc2c903694161d4ca38ca4
JWulf <JSW@JSWulf.com>
2018-04-04 12:18:17 -0700

    t540p

---
+++ Win10Migrate/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win10Migrate
+{
+    public static class Extensions
+    {
+        public static bool ContainsIgnoreCase(this String baseString, string Compare)
+        {
+            if (baseString.IndexOf(Compare, StringComparison.InvariantCultureIgnoreCase) >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public static bool EndsWithIgnoreCase(this String baseString, string Compare)
+        {
+            if (baseString.EndsWith(Compare, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
