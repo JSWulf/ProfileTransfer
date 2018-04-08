b2f7cfa22373e9bfb991f7d61852864e62740de0
Joseph Wulf <JSW@JSWulf.com>
2018-04-08 12:20:41 -0700

    checkin

--- Win10Migrate/Extensions.cs
+++ Win10Migrate/Extensions.cs
@@ -54,18 +54,7 @@ namespace Win10Migrate
             // Return total size
             return fileOutput;
         }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Win10Migrate
-{
-    public static class Extensions
-    {
+    
         public static bool ContainsIgnoreCase(this String baseString, string Compare)
         {
             if (baseString.IndexOf(Compare, StringComparison.InvariantCultureIgnoreCase) >= 0)
